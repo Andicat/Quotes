b5ee733707ef7f764f1ba120a27ba4e78f3e95c7
Katerina <andicat@mail.ru>
2019-11-16 14:13:32 +0300

    social

--- js/script.js
+++ js/script.js
@@ -27,7 +27,6 @@
 
   function onClickSocialTwitter(evt) {
     this.href = 'http://twitter.com/share?url=' +  window.location.href + '&text=' + quoteText.textContent;
-    //document.title+'&url='+window.location.href+window.location.hash+'&via=+ссылка на Twitter-аккаунт, например официальный канал сайта">
   }
 
   function onClickSocialVk(evt) {
@@ -35,7 +34,7 @@
   }
 
   function onClickSocialFb(evt) {
-    this.href = 'http://www.facebook.com/share.php?url=' + window.location.href;
+    this.href = 'http://www.facebook.com/share.php?u=' + window.location.href;
   }
 
   function showQuote(data) {
